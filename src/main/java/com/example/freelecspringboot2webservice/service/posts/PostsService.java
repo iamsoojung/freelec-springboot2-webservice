253a3f5eed51d2ae22350d15568b6c5efc2faf77
iamsoojung <crystar2402@naver.com>
2023-08-20 20:27:30 +0900

    게시글 생성, 조회, 수정 화면 및 API 생성

--- src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.freelecspringboot2webservice.service.posts;
 
 import com.example.freelecspringboot2webservice.domain.posts.Posts;
 import com.example.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.example.freelecspringboot2webservice.web.dto.PostsListResponseDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -37,5 +41,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
