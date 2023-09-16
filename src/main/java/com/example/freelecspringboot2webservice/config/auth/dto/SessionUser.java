25a44c0205c1475c72aa23761803d913b7b9e6bc
iamsoojung <crystar2402@naver.com>
2023-09-16 15:53:14 +0900

    Feat: 로그인 기능 내 spring security 생성

---
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.example.freelecspringboot2webservice.config.auth.dto;
+
+import com.example.freelecspringboot2webservice.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
